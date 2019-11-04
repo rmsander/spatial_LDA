61d8d7e66617707bd80cdbf4b09d1cf78ee72ae7
Crystal Wang <cwang506@mit.edu>
2019-11-04 10:52:34 -0500

    added feature_Extraction

--- python/feature_extraction.py
+++ python/feature_extraction.py
@@ -4,7 +4,7 @@ import os
 from sklearn.cluster import MiniBatchKMeans
 from sklearn.neural_network import MLPClassifier
 
-img_path = '/Users/crystalwang/Downloads/test.png'
+img_path = '/Users/crystalwang/Documents/test.png'
 
 def get_feature_vector(img):
     sift = cv.xfeatures2d_SIFT.create()
@@ -13,9 +13,13 @@ def get_feature_vector(img):
 
 def main():
     img = cv.imread(img_path)
+    img2 = img
     kp, des = get_feature_vector(img)
     print(kp)
     print(des)
+    img2=cv.drawKeypoints(img,kp,img2)
+    cv.imwrite("/Users/crystalwang/Documents/test_keypoints.png", img2)
+    
 
 if __name__ == "__main__":
     main()
