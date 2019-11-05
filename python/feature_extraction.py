2884f23736d067acf2d4e3220aff40c08fe078a7
Crystal Wang <cwang506@mit.edu>
2019-11-04 19:38:03 -0500

    added minor things

--- python/feature_extraction.py
+++ python/feature_extraction.py
@@ -15,7 +15,7 @@ def main():
     img = cv.imread(img_path)
     img2 = img
     kp, des = get_feature_vector(img)
-    # print(kp)
+    print(kp)
     print(des)
     print(des.shape)
     img2=cv.drawKeypoints(img,kp,img2)
