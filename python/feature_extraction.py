4ea6144e7d04e1b4695d2c7f53ab7671e743e0df
Crystal Wang <cwang506@mit.edu>
2019-11-04 22:39:40 -0500

    conditioned on None

--- python/feature_extraction.py
+++ python/feature_extraction.py
@@ -43,7 +43,7 @@ def create_feature_matrix(img_path, n_clusters=800):
     #     descriptor_list_dic[f]= des
     # with open("/home/yaatehr/programs/spatial_LDA/data/img_descriptors_dic.pkl", "wb") as f:
     #     pickle.dump(descriptor_list_dic, f)
-    kmeans.fit([i for i in list(descriptor_list_dic.values()) if i not None])
+    kmeans.fit([i for i in list(descriptor_list_dic.values()) if i != None])
 
     # Get image files
     M = []
