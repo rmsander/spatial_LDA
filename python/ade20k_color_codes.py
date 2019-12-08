1f4e7043727a327543863f3c1d3878ae6a416e90
rmsander <rmsander@mit.edu>
2019-12-08 12:46:57 -0500

    Added main method

--- python/ade20k_color_codes.py
+++ python/ade20k_color_codes.py
@@ -2,12 +2,17 @@ import json
 import numpy as np
 import os
 
-F_JSON = os.path.join("..", "..", "datasets", "seg_data", "tree.json")
-print("Tree located at: {}".format(F_JSON))
 
-# Read json file to create dict of label --> root
-with open(F_JSON) as json_file:
-    data = json.load(json_file)
-    json_file.close()
+def main():
+    F_JSON = os.path.join("..", "..", "datasets", "seg_data", "tree.json")
+    print("Tree located at: {}".format(F_JSON))
 
-print("JSON KEYS: {}".format(json_file))
+    # Read json file to create dict of label --> root
+    with open(F_JSON) as json_file:
+        data = json.load(json_file)
+        json_file.close()
+
+    print("JSON KEYS: {}".format(json_file))
+
+if __name__=="__main__":
+    main()
