0c440bbdb1ebd41d5f4756204b9758394031f489
Yaateh Richardson <yaatehr@mit.edu>
2019-12-08 21:02:37 -0500

    update params

--- python/feature_extraction.py
+++ python/feature_extraction.py
@@ -18,7 +18,7 @@ import torch
 from tqdm import tqdm
 import gc
 
-n_keypoints = 300  # hyperparameter, need to tune
+n_keypoints = 500  # hyperparameter, need to tune
 n_cnn_keypoints = 4 * 49
 n_clusters = 300  # also need to tune this
 feature_model = "resnet34" # one of resnet34 TODO resnet 18, inception net
